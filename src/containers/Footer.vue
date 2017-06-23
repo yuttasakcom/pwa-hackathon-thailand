b5976c90d325823e2f574ac5b8bff54357dc4b91
YoProgrammer <yuttasakcom@gmail.com>
2017-06-24 02:25:41 +0700

    Add sponsors to footer

--- src/containers/Footer.vue
+++ src/containers/Footer.vue
@@ -1,14 +1,28 @@
 <template>
-  <div>
+  <div class="container">
     <h4 class="text-center pt-5">Sponsors</h4>
-    <div class="col-lg-12 text-center pb-2">
-      <img class="sponsors" src="/static/img/sponsors/pwa-hackathon-thailand.png" alt="">
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors pwa" src="/static/img/sponsors/pwa-hackathon-thailand.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors google" src="/static/img/sponsors/google-developer.120759fc.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors firebase" src="/static/img/sponsors/firebase.745f65de.png" alt="">
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-img.sponsors {
+img.sponsors.pwa {
   width: 5em;
+}
+img.sponsors.google {
+  width:20em;
+}
+img.sponsors.firebase {
+  width: 10em;
 }
 </style>
