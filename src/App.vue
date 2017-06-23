3044970d0ab899cd1ef9848fb70df25512e86332
YoProgrammer <yuttasakcom@gmail.com>
2017-06-23 22:21:30 +0700

    Create containers [Navbar, Footer]

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,18 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <router-view></router-view>
-    </main>
+    <Navbar></Navbar>
+    <router-view></router-view>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import Navbar from '@/containers/Navbar'
+  import Footer from '@/containers/Footer'
+  export default {
+    components: {
+      Navbar,
+      Footer
+    }
+  }
 </script>
-
-<style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #4fc08d;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
-</style>
