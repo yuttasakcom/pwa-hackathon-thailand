880c23227e0d8205a8a141cc756e74a72caf0cd6
YoProgrammer <yuttasakcom@gmail.com>
2017-06-24 02:08:35 +0700

    Add sponsors to footer

--- src/containers/Footer.vue
+++ src/containers/Footer.vue
@@ -1,3 +1,14 @@
 <template>
-  <div>footer</div>
+  <div>
+    <h4 class="text-center pt-5">Sponsors</h4>
+    <div class="col-lg-12 text-center pb-2">
+      <img class="sponsors" src="/static/img/sponsors/pwa-hackathon-thailand.png" alt="">
+    </div>
+  </div>
 </template>
+
+<style scoped>
+img.sponsors {
+  width: 5em;
+}
+</style>
