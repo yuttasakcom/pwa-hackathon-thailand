8fe573688260f093be293e6f237659f3fa493535
YoProgrammer <yuttasakcom@gmail.com>
2017-06-24 03:05:23 +0700

    Add sponsors to footer

--- src/containers/Footer.vue
+++ src/containers/Footer.vue
@@ -1,28 +1,78 @@
 <template>
   <div class="container">
-    <h4 class="text-center pt-5">Sponsors</h4>
+    <h3 class="text-center pt-5 pb-3">Sponsors</h3>
     <div class="row">
       <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
-        <img class="sponsors pwa" src="/static/img/sponsors/pwa-hackathon-thailand.png" alt="">
+        <img class="sponsors fiveem white_bg" src="/static/img/sponsors/pwa-hackathon-thailand.png" alt="">
       </div>
       <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
-        <img class="sponsors google" src="/static/img/sponsors/google-developer.120759fc.png" alt="">
+        <img class="sponsors twentyem white_bg" src="/static/img/sponsors/google-developer.120759fc.png" alt="">
       </div>
       <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
-        <img class="sponsors firebase" src="/static/img/sponsors/firebase.745f65de.png" alt="">
+        <img class="sponsors tenem white_bg" src="/static/img/sponsors/firebase.745f65de.png" alt="">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors fifteenem white_bg" src="/static/img/sponsors/sellsuki.bb84d5bc.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem white_bg" src="/static/img/sponsors/sprint3r.d2c12bbb.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem white_bg" src="/static/img/sponsors/tencent.a4fdb218.jpg" alt="">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem" src="/static/img/sponsors/donut.b9d76d2e.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem white_bg" src="/static/img/sponsors/3ds.9af5b385.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem" src="/static/img/sponsors/nextzy.4a2483da.png" alt="">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem" src="/static/img/sponsors/pantip.c1a8cd99.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem white_bg" src="/static/img/sponsors/thothzocial.6178af66.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem" src="/static/img/sponsors/cleverse.77b991d3.png" alt="">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem white_bg" src="/static/img/sponsors/wongnai.2776d7dd.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors tenem white_bg" src="/static/img/sponsors/skootar.65eb5ddf.png" alt="">
+      </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 text-center pb-2">
+        <img class="sponsors fiveem" src="/static/img/sponsors/logo_papakuma_bgwhi.c8eda24f.png" alt="">
       </div>
     </div>
   </div>
 </template>
 
 <style scoped>
-img.sponsors.pwa {
+img.sponsors.fiveem {
   width: 5em;
 }
-img.sponsors.google {
-  width:20em;
-}
-img.sponsors.firebase {
+img.sponsors.tenem {
   width: 10em;
+}
+.white_bg {
+  background-color: #fff;
+}
+img.sponsors.fifteenem {
+  width: 15em;
+}
+img.sponsors.twentyem {
+  width:20em;
 }
 </style>
