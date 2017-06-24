148c58ba09abd3db1845bf4294ead2f732f06fa6
YoProgrammer <yuttasakcom@gmail.com>
2017-06-24 12:10:44 +0700

    Create SigninButton Component

---
+++ src/components/SigninButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button type="button" :class="`btn btn-outline-${login.btnType}`" @click="signIn"><i :class="`${login.btnIcon}`" aria-hidden="true"></i>{{login.btnMsg}}</button>
+  </div>
+</template>
+
+<script>
+  import { SIGNIN } from '@/services/Auth'
+  export default {
+    props: ['login'],
+    methods: {
+      signIn () {
+        SIGNIN(this.login.btnProvider).then(res => {
+          this.$router.replace('/choose/character')
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+button {
+  cursor: pointer;
+  width: 14em;
+  margin-bottom: 10px;
+}
+i {
+  padding-right: 7px;
+}
+</style>
