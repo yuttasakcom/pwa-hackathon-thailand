1e11a3a62311c9ad046b07c432eab0b8c6231820
YoProgrammer <yuttasakcom@gmail.com>
2017-06-25 13:32:26 +0700

    Create Sprite Component

---
+++ src/components/Sprite.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <div class="sprite" :style="`height:${character.height}px;`">
+      <div class="slide" :style="`
+        -webkit-animation-duration:${speed}ms;
+        -moz-animation-duration:${speed}ms;
+        -webkit-animation-name:animate_${character.height}_frame_${character.frame};
+        -moz-animation-name:animate_${character.height}_frame_${character.frame};
+      `">
+      <img
+        :src="`/static/img/sprites/${character.name}/${character.action}_${character.position}/${n}.png`"
+        :height="character.height"
+        class="displayed"
+        v-for="n in character.frame">
+      </div>
+    </div>
+    <div class="name">{{character.msg}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['character'],
+    computed: {
+      speed () {
+        switch (this.character.speed) {
+          case 1:
+            return 1000
+          case 2:
+            return 800
+          case 3:
+            return 600
+          case 4:
+            return 400
+          case 5:
+            return 200
+          default:
+            return 1000
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+img.displayed {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.sprite {
+  position: relative;
+  overflow: hidden;
+}
+.slide {
+  position: relative;
+  -webkit-animation-iteration-count:infinite;
+  -webkit-animation-timing-function:step-start;
+  -webkit-animation-direction:forward;
+  -moz-animation-iteration-count:infinite;
+  -moz-animation-timing-function:step-start;
+  -moz-animation-direction:forward;
+}
+@keyframes animate_200_frame_10 {
+  0%   { top:0; }
+  10%  { top:-200px; }
+  20%  { top:-400px; }
+  30%  { top:-600px; }
+  40%  { top:-800px; }
+  50%  { top:-1000px; }
+  60%  { top:-1200px; }
+  70%  { top:-1400px;}
+  80%  { top:-1600px; }
+  90%  { top:-1800px; }
+  100% { top:0 }
+}
+@keyframes animate_190_frame_10 {
+  0%   { top:0; }
+  10%  { top:-190px; }
+  20%  { top:-380px; }
+  30%  { top:-570px; }
+  40%  { top:-760px; }
+  50%  { top:-950px; }
+  60%  { top:-1140px; }
+  70%  { top:-1330px;}
+  80%  { top:-1520px; }
+  90%  { top:-1710px; }
+  100% { top:0 }
+}
+</style>
--- src/pages/ChooseCharacter.vue
+++ src/pages/ChooseCharacter.vue
@@ -1,17 +1,74 @@
 <template>
-  <div class="contianer">
-    <div class="pt-lg-30 text-center">
-      กำลังพัฒนา...
+  <div class="container">
+    <div class="pt-lg-30 pt-md-30 pt-sm-30 text-center">
+      <h3 style="color:black">กำลังพัฒนา</h3>
+      <button class="btn btn-primary" @click="selectMap">เลือกแผนที่</button>
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+          <Sprite :character="characters.player1"></Sprite>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+          <Sprite :character="characters.player2"></Sprite>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+  import Sprite from '@/components/Sprite'
   export default {
     created () {
       this.$store.dispatch('setCss', {
-        bg: "background: url('/static/img/background/bg_green1.jpg') center center / cover no-repeat fixed;"
+        bg: "background: url('/static/img/background/HNK-1.gif') bottom / cover no-repeat fixed;"
       })
+    },
+    data: () => ({
+      map: true,
+      characters: {
+        player1: {
+          name: 'knight',
+          msg: 'นักรบมังกร',
+          height: '200',
+          position: 'left',
+          action: 'idle',
+          speed: 1,
+          frame: 10
+        },
+        player2: {
+          name: 'ninja',
+          msg: 'นักฆ่าหั่นศพ',
+          height: '190',
+          position: 'right',
+          action: 'idle',
+          speed: 1,
+          frame: 10
+        }
+      }
+    }),
+    components: {
+      Sprite
+    },
+    methods: {
+      selectMap () {
+        if (this.map) {
+          this.$store.dispatch('setCss', {
+            bg: "background: url('/static/img/background/83312792.gif') center center / cover no-repeat fixed;"
+          })
+          this.map = false
+        } else {
+          this.$store.dispatch('setCss', {
+            bg: "background: url('/static/img/background/HNK-1.gif') bottom / cover no-repeat fixed;"
+          })
+          this.map = true
+        }
+      }
     }
   }
 </script>
+
+<style scoped>
+.btn {
+  cursor: pointer;
+}
+</style>
